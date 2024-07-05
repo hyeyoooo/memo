b7a151f7ffee5d5bea1c411ffb9d6305958d6f09
Hyerin <12qwpo50@gmail.com>
2024-07-05 19:40:18 +0900

    Create, Read 구현

---
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -0,0 +1,95 @@
+package com.sparta.memo.controller;
+
+import com.sparta.memo.dto.MemoRequestDto;
+import com.sparta.memo.dto.MemoResponseDto;
+import com.sparta.memo.entity.Memo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class MemoController {
+
+    //Long : ID, Memo : 실제 데이터
+    private final Map<Long, Memo> memoList = new HashMap<>();
+
+
+    @PostMapping("/memos")
+    public MemoResponseDto creatMemo(@RequestBody MemoRequestDto requestDto) {
+        // RequestDto -> Entity
+        Memo memo = new Memo(requestDto);
+
+        // Memo Max Id Check : 아이디 중복 방지 체크
+        // 중복 방지를 위해 값에 +1을 해주고 아니라면 1부터 시작하도록 1을 대입
+        Long maxId = memoList.size() > 0 ? Collections.max(memoList.keySet()) + 1 : 1;
+        memo.setId(maxId);
+
+        // DB 저장
+        memoList.put(memo.getId(), memo);
+
+        // Entity -> ResponseDto
+        MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
+
+        return memoResponseDto;
+    }
+
+    @GetMapping("/memos")
+    public List<MemoResponseDto> getMemos() {
+        // Map to List
+        List<MemoResponseDto> responseList = memoList.values().stream()
+                .map(MemoResponseDto::new).toList();
+
+        return responseList;
+    }
+
+}
+
+//package com.sparta.memo.controller;
+//
+//import com.sparta.memo.dto.MemoRequestDto;
+//import com.sparta.memo.dto.MemoResponseDto;
+//import com.sparta.memo.entity.Memo;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.Collections;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//
+//@RestController
+//@RequestMapping("/api")
+//public class MemoController {
+//
+//    private final Map<Long, Memo> memoList = new HashMap<>();
+//
+//    @PostMapping("/memos")
+//    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
+//        // RequestDto -> Entity
+//        Memo memo = new Memo(requestDto);
+//
+//        // Memo Max ID Check
+//        Long maxId = memoList.size() > 0 ? Collections.max(memoList.keySet()) + 1 : 1;
+//        memo.setId(maxId);
+//
+//        // DB 저장
+//        memoList.put(memo.getId(), memo);
+//
+//        // Entity -> ResponseDto
+//        MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
+//
+//        return memoResponseDto;
+//    }
+//
+//    @GetMapping("/memos")
+//    public List<MemoResponseDto> getMemos() {
+//        // Map To List
+//        List<MemoResponseDto> responseList = memoList.values().stream()
+//                .map(MemoResponseDto::new).toList();
+//
+//        return responseList;
+//    }
+//}
